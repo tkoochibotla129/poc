0fdcd6eec37472803835522b805c7e7ed5bcc349
pc <pc@DESKTOP-P81C6JP>
2020-04-14 00:14:59 -0700

    Test

---
+++ POC/src/main/java/POC/tst/App.java
@@ -0,0 +1,13 @@
+package POC.tst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
